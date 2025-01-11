c7132e6b75e8538387d98ff9440233f38d73fa4b
radhitya <radhitya@temanbsd.com>
2025-01-11 13:44:40 +0700

    la luna

--- main.c
+++ main.c
@@ -6,47 +6,58 @@ FILE *fptr;
 
 void sf() {
     fptr = fopen("data.txt", "rb+");
-    if ( fptr == NULL ) {
-        printf("OK! data.txt\n");
+    if (fptr == NULL) {
+        printf("Sukses buat data.txt\n");
         fptr = fopen("data.txt", "wb");
+        if (fptr == NULL) {
+            perror("Galat buat data.txt");
+            exit(1);
+        }
     }
 }
 
-/*
-void parser(char * s) {
-    int n = strlen(s);
-    int start = 0, end = 0;
-    int i, j;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Penggunaan: %s <berkas_input>\n", argv[0]);
+        return 1;
+    }
 
-    for(i = 0; i < n; i++) {
-        if(s[i] == '_') {
-
-}
-*/
-
-int main(int argc, char * argv[]) {
     sf();
 
-    if(argc == 1) {
-        printf("not enough parameter\n");
-        return -1;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp("-v", argv[i]) == 0) {
+            printf("ini -v\n");
+        }
     }
 
     char *txt = malloc(1024);
+    if (txt == NULL) {
+        perror("Galat alokasi memori");
+        return 1;
+    }
 
     fptr = fopen(argv[1], "rt");
+    if (fptr == NULL) {
+        perror("Galat membuka masukan berkas");
+        free(txt);
+        return 1;
+    }
 
     FILE *fpto = fopen("data.html", "wb");
+    if (fpto == NULL) {
+        perror("Galat membuat keluaran berkas");
+        fclose(fptr);
+        free(txt);
+        return 1;
+    }
 
-    while(fgets(txt, 1024, fptr ) != NULL) {
-        for(int i = 0; i < strlen(txt); i++) {
-            if(txt[i] == '_') {
-            }
+    while (fgets(txt, 1024, fptr) != NULL) {
         fprintf(fpto, "<p>%s</p>\n", txt);
-        }
+    }
 
     fclose(fptr);
     fclose(fpto);
     free(txt);
     return 0;
 }
+
